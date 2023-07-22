52e0ee76f539d4865e3d1affb7a5a20dc3478a03
Tobias Fenner <tfenner@usc.edu>
2023-07-22 13:35:30 -0700

    styled better

--- bruh/src/FlightDateInput.jsx
+++ bruh/src/FlightDateInput.jsx
@@ -9,13 +9,19 @@ const FlightDateInput = () => {
   };
 
   return (
-    <div className="flight-date-input">
+    <div className="flight-input">
       <h2>Select Your Flight Date</h2>
       <input
         type="date"
         value={selectedDate ? format(selectedDate, 'yyyy-MM-dd') : ''}
         onChange={(e) => handleDateChange(new Date(e.target.value))}
-        className="input-style"
+        style={{
+          fontSize: '18px',
+          height: '40px',
+          width: '200px',
+          borderRadius: '10px',
+          padding: '8px',
+        }}
       />
     </div>
   );
