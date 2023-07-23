263978f022311fa9f43c5b11cfecff840a16f74a
Tobias Fenner <tfenner@usc.edu>
2023-07-22 19:01:36 -0700

    console AA3203 we have response
    HOUSTON

--- bruh/src/FlightList.jsx
+++ bruh/src/FlightList.jsx
@@ -6,14 +6,14 @@ import axios from 'axios';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDtXJdCDFnkS044k-_6TMrd83YQHo-NX04",
-  authDomain: "flites.firebaseapp.com",
-  projectId: "flites",
-  storageBucket: "flites.appspot.com",
-  messagingSenderId: "438351659234",
-  appId: "1:438351659234:web:c110c8410d5c9300e55ebb"
-};
-  
+    apiKey: "AIzaSyDtXJdCDFnkS044k-_6TMrd83YQHo-NX04",
+    authDomain: "flites.firebaseapp.com",
+    projectId: "flites",
+    storageBucket: "flites.appspot.com",
+    messagingSenderId: "438351659234",
+    appId: "1:438351659234:web:c110c8410d5c9300e55ebb"
+  };
+
 initializeApp(firebaseConfig);
 
 const FlightList = () => {
@@ -25,7 +25,6 @@ const FlightList = () => {
     const database = getDatabase();
     const flightsRef = ref(database, 'flights');
 
-    // Fetch the data from Firebase Realtime Database and set it in the state
     const fetchData = () => {
       onValue(flightsRef, (snapshot) => {
         const data = snapshot.val();
@@ -41,22 +40,19 @@ const FlightList = () => {
       });
     };
 
-    fetchData(); // Initial fetch
+    fetchData();
 
-    // Listen for changes in the database to trigger a refresh
     const dataChangeCallback = () => {
       fetchData();
     };
     onValue(flightsRef, dataChangeCallback);
 
-    // Clean up the event listener when the component is unmounted
     return () => {
       off(flightsRef, dataChangeCallback);
     };
   }, []);
 
   useEffect(() => {
-    // Function to check for duplicate flights
     const findDuplicateFlights = (flightsArray) => {
       const duplicateMap = {};
       const duplicateIds = new Set();
@@ -64,8 +60,8 @@ const FlightList = () => {
       flightsArray.forEach((flight) => {
         const flightKey = `${flight.flightNumber}-${flight.flightDate}`;
         if (duplicateMap[flightKey]) {
-          duplicateIds.add(flightKey); // Use flightKey instead of flight.id
-          duplicateMap[flightKey].push(flight.id); // Use flightKey instead of flight.id
+          duplicateIds.add(flightKey);
+          duplicateMap[flightKey].push(flight.id);
         } else {
           duplicateMap[flightKey] = [flight.id];
         }
@@ -77,26 +73,44 @@ const FlightList = () => {
     findDuplicateFlights(flights);
   }, [flights]);
 
-  // API Call for Testing
- // API Call for Testing
- const apiCall = async (method, params, cb) => {
-    params.api_key = "dcb6ad7a-9efc-4bc2-b038-709e397984d4";
+  const fetchFlightInformation = async (flightIata) => {
+    const api_key = "dcb6ad7a-9efc-4bc2-b038-709e397984d4";
     const api_base = "https://airlabs.co/api/v9/";
     try {
-      const response = await axios.post(`${api_base}${method}`, params);
-      cb(null, response);
+      const response = await axios.get(`${api_base}flight`, {
+        params: {
+          api_key,
+          flight_iata: flightIata,
+        },
+      });
+      console.log(`API Call - Flight Number: ${flightIata} - Response:`, response.data); // Debug: Log API response
+      return response.data;
     } catch (error) {
-      cb(error, null);
+      console.error('Error fetching flight information:', error);
+      return null;
     }
   };
 
-  apiCall('ping', { param1: 'value1' }, (err, res) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(res);
-    }
-  });
+  useEffect(() => {
+    const fetchFlightInfoForFlights = async () => {
+      const updatedArrivalTimes = {};
+      for (const flight of flights) {
+        const { flightNumber, flightDate } = flight;
+        console.log(`Fetching Arrival Time for Flight Number: ${flightNumber} - Date: ${flightDate}`); // Debug: Log flight details
+        const flightInfo = await fetchFlightInformation(flightNumber);
+        if (flightInfo) {
+          console.log(`Arrival Time for Flight Number: ${flightNumber} - Date: ${flightDate} - ${flightInfo.arr_time_utc}`); // Debug: Log arrival time
+          updatedArrivalTimes[`${flightNumber}-${flightDate}`] = flightInfo.arr_time_utc;
+        } else {
+          console.log(`No Arrival Time found for Flight Number: ${flightNumber} - Date: ${flightDate}`); // Debug: Log if no arrival time
+          updatedArrivalTimes[`${flightNumber}-${flightDate}`] = 'N/A';
+        }
+      }
+      setArrivalTimes(updatedArrivalTimes);
+    };
+
+    fetchFlightInfoForFlights();
+  }, [flights]);
 
   return (
     <div>
@@ -111,9 +125,7 @@ const FlightList = () => {
               color: duplicateFlights.has(`${flight.flightNumber}-${flight.flightDate}`) ? 'red' : 'black',
             }}
           >
-            {/* Flight Number: {flight.flightNumber}, Date: {flight.flightDate}, Arrival Time: {arrivalTimes[`${flight.flightNumber}-${flight.flightDate}`]} */}
-            Flight Number: {flight.flightNumber}, Date: {flight.flightDate}
-
+            Flight Number: {flight.flightNumber}, Date: {flight.flightDate}, Arrival Time: {arrivalTimes[`${flight.flightNumber}-${flight.flightDate}`] || 'N/A'}
           </li>
         ))}
       </ul>
